93e9e9ae7cdcc27df560ea5263471ba370e1caff
Sonali <pv.sonali.official@gmail.com>
2025-05-28 19:44:44 +0530

    Database schema created

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
 
 const CommentSchema = new mongoose.Schema({
-    postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
-    username: { type: String, required: true },
-    text: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
+    username: { 
+        type: String, 
+        required: true 
+    },
+    postId: {
+        type: String,
+        required: true
+    },
+    comments: {
+        type: String,
+        required: true
+    },
+    createdAt: { 
+        type: Date, 
+        default: Date.now }
 });
 
 module.exports = mongoose.model('Comment', CommentSchema);
--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const commentRoutes = require('./routes/commentRoutes');
 const uploadRoutes = require('./routes/uploadRoutes');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 dotenv.config();
 app.use(cors())
